0646ba2fef0daa0d7836463275db4b334a7e230f
Kenneth Jiang <kenneth@rhumbix.com>
2016-03-31 18:17:50 -0700

    pagination

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'omniauth-facebook'
 gem 'paperclip'
 gem 'aws-sdk', '< 2.0'
 
-
+gem 'kaminari'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,7 @@ class PhotosController < ApplicationController
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.includes(:user).all
+    @photos = Photo.includes(:user).page(params[:page]).per(6)
   end
 
   # GET /photos/1
